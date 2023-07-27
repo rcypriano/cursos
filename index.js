35191c2e9ba17fccebbf68c272fbc21b9a65952e
rcypriano <rccypriano@gmail.com>
2023-07-26 21:57:29 -0300

    conclusão todolist

--- index.js
+++ index.js
@@ -1,47 +1,78 @@
 // pegando elementos do html pelo atributo
 const formulario = document.getElementById('form')
 const todoInput = document.getElementById('todo')
-const todoList = []
 const lista = document.getElementById('list')
-
+const CHAVE_DADOS_OFFLINE = "TODOLIST";
+const todoList = pegarDadosOffline();
+renderizarLista()
 // se inscrevendo no evento de submit do formulário
 formulario.addEventListener('submit', function (event) {
-event.preventDefault() // impedindo o comportamento padrão
+    event.preventDefault() // impedindo o comportamento padrão
+    const value = todoInput.value.trim()
+    if (value === '') {
+        alert('Campo em branco!')
+        return;
+    }
 
-const value = todoInput.value.trim()
-if (value === '') {
-alert('Campo em branco!')
-return;
-}
+    //mapeando lista de dados e convertendo tudo pra maiúscula
+    const todoListUppercase = todoList.map(function (item) {
+        return item.toUpperCase();
+    });
 
-//mapeando lista de dados e convertendo tudo pra maiúscula
-const todoListUppercase = todoList.map(function (item) {
-    return item.toUpperCase();
-});
+    //verificando se o dado já está incluso na list - caps sensitive
+    const jaExisteNaPilha = todoListUppercase.includes(value.toUpperCase());
+    if (jaExisteNaPilha) {
+        alert("O valor já foi inserido!");
+        return;
+    }
 
-//verificando se o dado já está incluso na list - caps sensitive
-const jaExisteNaPilha = todoListUppercase.includes(value.toUpperCase());
-if (jaExisteNaPilha) {
-    alert("O valor já foi inserido!");
-    return;
-}
+    todoList.push(value)
 
-todoList.push(value)
+    todoInput.value = '' // limpando o atributo pra botar mais dado depois etc
+    renderizarLista()
 
-todoInput.value = '' // limpando o atributo pra botar mais dado depois etc
+})
 
-console.log(value)
-lista.innerHTML = ''
+function renderizarLista() {
+    lista.innerHTML = ''
 
-for (let index = 0; index <todoList.length; index += 1) { //percorrendo a lista item a item (loop)
-    const itemDaLista = todoList[index]
-    //adicionando o elemento no html referente ao item atual - lá pro user
-    lista.innerHTML += `
+    for (let index = 0; index < todoList.length; index += 1) { //percorrendo a lista item a item (loop)
+        const itemDaLista = todoList[index]
+        //adicionando o elemento no html referente ao item atual - lá pro user
+        lista.innerHTML += `
         <div class ="todo-item">
             <span>${itemDaLista}</span>
             <button type="button" onclick="removerTodoItem(${index})">x</button>
         </div>
     `;
+    }
+    salvarOffline()
 }
 
-})
+function removerTodoItem(index) {
+    const todoItem = todoList[index]
+    const isDeletar = confirm(`Deseja realmente remover a tarefa "${todoItem}"?`)
+    if (isDeletar) {
+        todoList.splice(index, 1)
+        console.log(todoList)
+    }
+    renderizarLista()
+}
+
+function salvarOffline() {
+    //convertendo array pra string pra depois voltar o string pra array
+    const todoListString = JSON.stringify(todoList)
+    localStorage.setItem(CHAVE_DADOS_OFFLINE, todoListString)
+}
+
+function pegarDadosOffline() {
+    const dadosOffline = localStorage.getItem(CHAVE_DADOS_OFFLINE)
+    console.log('salvo no navegador', dadosOffline)
+
+    if (dadosOffline === null) return []
+
+    const array = JSON.parse(dadosOffline)
+    console.log('depois de ser convertido de string para array', array)
+
+    return array
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,9 @@
 # Todo list
+
+Uma simples lista de tarefa para estudos.
+
+## Recursos
+- Uso do `localStorage` para armazenar, salvar e buscar os dados.
+- Botão para remover as tarefas com confirmação do usuário.
+- Validamos as tarefas para impedir a inserção de 2 tarefas com o mesmo nome
+  > esta validação ignora `uppercase` e `lowercase`
